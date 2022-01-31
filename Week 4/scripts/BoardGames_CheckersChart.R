ba58ef429bb149acf0a5e0216567c1dee29358d0
Dan <daniel_fellowes@hotmail.co.uk>
2022-01-31 21:14:23 +0000

    bugs fixed

--- Week 4/scripts/BoardGames_CheckersChart.R
+++ Week 4/scripts/BoardGames_CheckersChart.R
@@ -19,6 +19,8 @@ details <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/ti
 board_games <- details %>%
   left_join(y = ratings, by = "id")
 
+overall_median <- median(board_games$average)
+
 # split designer column into multiple rows
 bg_split <- board_games %>%
   mutate(boardgamedesigner = strsplit(gsub("\\[|\\'|\\]", "", gsub(", Jr.", " Jr.", boardgamedesigner)), ", "),
@@ -124,6 +126,9 @@ check_players <- merge(checkers_all, players, by=c("player_level_count", "groupe
 bg <- data.frame(x_amount = seq(1:6),
                  r = rep(4.5, 6))
 
+factors$chart_value <- seq(4,0.5, by = -0.5)
+factor_labels <- factors %>% arrange(chart_value)
+
 # CHARTS
 
 # produce main checkers chart
@@ -141,13 +146,13 @@ main <- check_players %>% ggplot() +
   annotate(geom = "richtext", x = 4, y = 4.8, label = player_list$label[4], family = "Bahnschrift", vjust = 1, color = "white") +
   annotate(geom = "richtext", x = 5, y = 4.8, label = player_list$label[5], family = "Bahnschrift", hjust = 1, color = "white") +
   annotate(geom = "richtext", x = 6, y = 4.8, label = player_list$label[6], family = "Bahnschrift", hjust = 1, color = "white") +
-  ggtitle("<span style = 'font-size: 45pt;'>MOST PRODUCTIVE BOARD GAME DESIGNERS</span><br>Exploration of the six board game designers with the most design credits.<br>", subtitle = "<b>Richard H. Berg</b>, <b>Joseph Miranda</b> and <b>Bruno Cathala</b> each achieved higher-than-average ratings on over half of their extensive catalogues,<br>with <b>Berg</b> earning the highest individual rating: 8.6/10 for GMT Games's 2019 American Civil War title, Death Valley: Battles for the Shenandoah.<br><b>Reiner Knizia</b> is far and away the most productive designer with an impressive 329 titles, but less than 30% achieved an average rating<br>higher than the median. <b>Jim Dunnigan</b> was the first of the 6 to produce a game, releasing Jutland for Avalon Hill in 1967 (6.7/10)<br><br><span style = 'font-size: 25pt;'>MEDIAN DECILE RATINGS vs.\nOVERALL MEDIAN RATING</span>") +
+  ggtitle("<span style = 'font-size: 45pt;'><b>MOST PRODUCTIVE BOARD GAME DESIGNERS</b></span><br>Exploration of the six board game designers with the most design credits.<br>", subtitle = "<br><span style = 'font-size: 25pt;'>MEDIAN DECILE RATINGS vs.\nOVERALL MEDIAN RATING</span>") +
   theme(plot.background = element_rect(fill = "white"),
         panel.background = element_rect(fill = "white"),
         plot.title.position = "plot",
-        plot.margin = margin(t = 0, r = 130, b = 0, l = 130, unit = "pt"),
+        plot.margin = margin(t = 80, r = 130, b = 30, l = 130, unit = "pt"),
         plot.title = element_markdown(hjust = 0.5, vjust = 0),
-        plot.subtitle = element_markdown(hjust = 0.5, vjust = 0, margin = margin(b = 20, unit = "pt")),
+        plot.subtitle = element_markdown(hjust = 0.5, vjust = 0, margin = margin(b = 40, unit = "pt")),
         legend.position = "none",
         panel.grid = element_line(color = "#edebe8"),
         legend.text = element_text(size = 11),
@@ -178,35 +183,36 @@ checkers_cart <- data.frame(x = c(10,
                                   6, 6, 
                                   7),
                             dummy = c(0,
-                                       0, 1,
-                                       0, 1, 1,
-                                       0, 0, 1, 1,
-                                       0, 0, 0, 0, 1,
-                                       0, 0, 0, 1,
-                                       0, 1, 1,
-                                       0, 1,
-                                       0)
+                                      0, 1,
+                                      0, 1, 1,
+                                      0, 0, 1, 1,
+                                      0, 0, 0, 0, 1,
+                                      0, 0, 0, 1,
+                                      0, 1, 1,
+                                      0, 1,
+                                      0)
                             
 )
 
 read_plot <- checkers_cart %>% ggplot(aes(x = x, y = y)) +
-  geom_point(aes(color = as.factor(dummy)), size = 12) +
+  geom_point(aes(color = as.factor(dummy)), size = 10) +
   scale_color_manual(values = c("#a8a5a2", "black")) +
   scale_y_continuous(name = "Variance %", labels = factor_labels$grouped, breaks = c(6, 5, 4, 3, 2, 1, 0, -1)) +
   ggtitle("HOW TO READ THE\nCHECKERS CHART", subtitle = "Every game a designer has produced\nis ranked and split evenly into 10 groups.\n\nEach dot represents one of these groups,\nwith the median rating of the group (decile)\nbeing grouped and plotted vs. the median rating\nacross all games in the dataset.\n\nThe closer to the centre, the higher the rating.") +
   geom_abline(intercept = 2.5, slope = 0) +     
   geom_text(aes(x = 16, y = 3.5, label = "MEDIAN\nRATING:\n6.45"), size = 3, family = "Bahnschrift") + 
-  expand_limits(y = c(-2, 8), x = c(5, 17)) +
+  expand_limits(y = c(-2, 8), x = c(3, 17)) +
   theme(plot.background = element_rect(fill = "white"),
         panel.background = element_rect(fill = "white"),
         plot.title = element_text(hjust = 0, face = "bold"),
+        plot.margin = margin(l = 80, b = 50, unit = "pt"),
         plot.title.position = "plot",
         plot.subtitle = element_text(hjust = 0, size = 15, margin = margin(b = 0, unit = "pt")),
         axis.title.x = element_blank(),
         axis.ticks.x = element_blank(),
         axis.text.x = element_blank(),
         legend.position = "none",
-        text = element_text(size = 20, family = "Bahnschrift"))
+        text = element_text(size = 17, family = "Bahnschrift"))
 
 # produce "Total Games By Designer" plot
 count_plot <- top_6_designers %>% 
@@ -220,6 +226,7 @@ count_plot <- top_6_designers %>%
   theme(panel.background = element_rect(fill = "white"),
         plot.background = element_rect(fill = "white"),
         plot.title.position = "plot",
+        plot.margin = margin(r = 80, l = 50, b = 40, unit = "pt"),
         plot.title = element_text(hjust = 0.5, face = "bold", margin = margin(t = 20, unit = "pt")),
         plot.subtitle = element_text(hjust = 0.5, size = 15),
         axis.title = element_blank(),
@@ -232,10 +239,10 @@ count_plot <- top_6_designers %>%
 library(gridExtra)
 
 # arrange subplots
-side <- grid.arrange(read_plot, count_plot, nrow = 1, widths = c(1,2.8))
+side <- grid.arrange(read_plot, count_plot, nrow = 1, widths = c(1,2.3))
 
 # arrange with main plot
-g <- arrangeGrob(main, side, nrow = 2, heights = c(2.5, 1))
+g <- arrangeGrob(main, side, nrow = 2, heights = c(2.8, 1))
 
 # save file
-ggsave(file="new_code_test.png", g, width = unit(18, "cm"), height = unit(25, "cm"))
+ggsave(file="checkers_chart.png", g, width = unit(18, "cm"), height = unit(25, "cm"))
